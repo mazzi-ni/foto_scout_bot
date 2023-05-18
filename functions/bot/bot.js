547bbb873039fa58858fe1ed1824d5da659dc88e
- Mazzi - <63593816+mazzi-ni@users.noreply.github.com>
2023-05-18 18:30:12 +0200

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -119,15 +119,14 @@ process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 // -------------------------------------------------------
 // function 
-
-// exports.handler = async event => {
-//   try {
-//     await bot.handleUpdate(JSON.parse(event.body))
-//     return { statusCode: 200, body: "" }
-//   } catch (e) {
-//     console.error("error in handler:", e)
-//     return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
-//   }
-// }
+exports.handler = async event => {
+   try {
+     await bot.handleUpdate(JSON.parse(event.body))
+     return { statusCode: 200, body: "" }
+   } catch (e) {
+     console.error("error in handler:", e)
+     return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
+   }
+}
 
 
